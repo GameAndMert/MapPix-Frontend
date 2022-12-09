e0a3c6d85511112444945004d964788be1d5abb1
GameAndMert <101985684+GameAndMert@users.noreply.github.com>
2022-12-09 16:40:21 +0300

    MapPix-Frontend

--- pages/constants/api.ts
+++
@@ -1,2 +0,0 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://localhost:8080';
-export const WS_URL  = process.env.NODE_ENV === 'development' ? 'ws://localhost:8080' : 'ws://localhost:8080';
